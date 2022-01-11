fd5ce6652833a2bb0b4ac371f836876e5e7042ab
mrkleiner <megaadrenaline1055@gmail.com>
2022-01-11 04:55:39 +0200

    we do a little bit of rewriting

--- baayxgj.markdown
+++
@@ -1,7 +0,0 @@
-BaaYXgj
--------
-
-
-A [Pen](https://codepen.io/mrkleiner/pen/BaaYXgj) by [MrKleiner](https://codepen.io/mrkleiner) on [CodePen](https://codepen.io).
-
-[License](https://codepen.io/mrkleiner/pen/BaaYXgj/license).
--- script.js
+++ script.js
@@ -1,19 +1,182 @@
-$(document).ready(function(){
-  $(".tut_head_button_splr").click(function(){
-    // $(".tut_step_content").toggleClass("tut_step_content_closed");
-    $(".tut_img").toggleClass("tut_step_content_slide_up");
-    $(".tut_step_content").toggleClass("tut_step_content_closed");
-     });
+window.blender_edit_mode = false
+
+
+document.addEventListener('click', event => {
+    console.log('click_registered');
   
-  $(".tut_head_button_splr_01").click(function(){
-    $(".tut_step_content_01").toggleClass("tut_step_content_closed");
-     });
-  
-  $(".tut_head_button_splr_02").click(function(){
-    $(".tut_step_content_02").toggleClass("tut_step_content_closed");
-     });
-  
-  $(".tut_head_button_splr_03").click(function(){
-    $(".tut_step_content_03").toggleClass("tut_step_content_closed");
-     });
+    // holster-unholster
+    const img_minimize = event.target.closest('.tut_img_button');
+    if (img_minimize) { img_toggler(img_minimize) }
+
+    // holster-unholster
+    const hattrick = event.target.closest('.navbar-icon');
+    if (hattrick) { activate_edit_mode(event) }
+
+	// enable border
+    const enableborder = event.target.closest('.enable_border_checkbox');
+    if (enableborder) { enable_border_top(enableborder) }
+
+	// enable border
+    const border_suggest = event.target.closest('.border_color_suggestion');
+    if (border_suggest) { set_border_top_col(border_suggest, true) }
+
+
+
+	// box mover
+    const boxmovert = event.target.closest('.article_movetop');
+    if (boxmovert) { box_mover(boxmovert, 't') }
+
+	// box mover
+    const boxmoverb = event.target.closest('.article_movebot');
+    if (boxmoverb) { box_mover(boxmoverb, 'b') }
+
 });
+
+document.addEventListener('change', event => {
+    console.log('change_registered');
+  
+    // border top colour
+    const btopcol = event.target.closest('.top_border_color_inp');
+    if (btopcol) { set_border_top_col(btopcol, false) }
+
+    // image preview
+    const imgprev = event.target.closest('.top_border_color_inp');
+    if (imgprev) { c_image_input(btopcol, false) }
+
+});
+
+
+
+
+
+
+function img_toggler(etgt)
+{
+    $(etgt).closest('.tut_step').find('img').toggleClass('e_hidden');
+}
+
+
+function activate_edit_mode(evee)
+{
+	if (evee.altKey)
+	{
+		if (window.blender_edit_mode == false)
+		{
+			window.blender_edit_mode = true
+			console.log('enter edit mode')
+			// make article header editable
+			$('.arcl_header_p').attr('contenteditable', true);
+			// make articles editable
+			$('.tut_step_head_text').attr('contenteditable', true);
+			var border_edit_m = 
+			`
+				<div class="at_border_edit_box">
+					<label class="enable_border_checkbox"><input checked type="checkbox" class="at_border_top_ena_ch">Enable border</label>
+					<input class="top_border_color_inp" type="color">
+					<div class="suggest_border_colors">
+						<div bgcol="#64DAFF" style="background-color: #64DAFF" class="border_color_suggestion"></div>
+						<div bgcol="#64FF86" style="background-color: #64FF86" class="border_color_suggestion"></div>
+						<div bgcol="#a064ff" style="background-color: #a064ff" class="border_color_suggestion"></div>
+					</div>
+					<div class="iliketomoveit">
+						<div class="article_movetop"><div class="mv_triangle"></div></div>
+						<div class="article_movebot"><div class="mv_triangle"></div></div>
+					</div>
+				</div>
+
+			`;
+			var img_adder = 
+			`
+				<div class="image_adder_btn"></div>
+			`;
+			var img_editor = 
+			`
+				<div class="image_editor">
+					<input class="c_image_input" type="file" accept=".png,.jpeg,.jpg,.jfif">
+					<input class="c_image_size" type="number" class="image_sizer">
+					<div class="image_deleter">Delete</div>
+				</div>
+			`;
+
+
+			$('.tut_step_head').append(border_edit_m);
+			$('.tut_step').append(img_adder);
+			$('.tut_step_content').append(img_editor);
+		}
+
+
+	}
+}
+
+// borders
+function enable_border_top(etgt)
+{
+	var cont = $(etgt).closest('.tut_step_head').find('.tut_step_head_text');
+	var ch = $(etgt).find('input');
+	console.log(cont)
+	console.log(ch)
+
+	// if border is there - set border width to 0
+	console.log(ch[0].checked)
+	if (!ch[0].checked)
+	{
+		$(cont).css('border-width', '0px');
+	}
+
+	// if not checked - try to pull previous state
+	if (ch[0].checked)
+	{
+		if (cont[0].hasAttribute('bwidth'))
+		{
+			$(cont).css('border-width', $(cont).attr('bwidth') + 'px');
+		}else{
+			$(cont).css('border-width', '5px');
+		}
+		
+	}
+}
+
+function set_border_top_col(etgt, sug)
+{
+
+	if(!sug)
+	{
+		$(etgt).closest('.tut_step_head').find('.tut_step_head_text').css('border-color', $(etgt).val());
+		
+	}
+	if(sug)
+	{
+		console.log($(etgt).css('background'))
+		$(etgt).closest('.tut_step_head').find('.tut_step_head_text').css('border-color', $(etgt).css('background-color'));
+		$(etgt).closest('.tut_step_head').find('.top_border_color_inp').val($(etgt).attr('bgcol'));
+	}
+
+}
+
+
+function box_mover(etgt, side)
+{
+	var soolja_box = $(etgt).closest('.tut_step')
+
+	console.log('where is ' + side)
+	
+	if (side == 't')
+	{
+		console.log('exec t');
+		soolja_box.insertBefore(soolja_box.prev());	
+	}
+	
+	if (side == 'b')
+	{
+		console.log('exec b');
+		soolja_box.insertAfter(soolja_box.next());	
+	}
+
+}
+
+
+
+function img_preview(etgt)
+{
+
+}
